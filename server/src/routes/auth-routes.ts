6236612301b5194933555a39f05b70a3aa883aa2
hazyplebian <michael.mangieri@yahoo.com>
2025-04-02 15:26:44 -0400

    completed auth-routes and auth.ts

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -3,9 +3,31 @@ import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
+
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
-};
+  const { username, password } = req.body;
+ 
+  const user = await User.findOne({ 
+    where: {
+      username,
+    },
+  });
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid username or password' });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: 'Invalid username or password' });
+  }
+
+  const secretKey = process.env.JWT_SECRET || '';
+
+  const token = jwt.sign({ username }, secretKey, {
+    expiresIn: '1h',})
+    return res.json({ token });
+  }
 
 const router = Router();
 
